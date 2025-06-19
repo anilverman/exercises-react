d9107e7f4ed9d9d5dc2bdd8d1d6dacb0754f897d
Anil Verman <anilverman14@gmail.com>
2025-06-18 21:44:27 -0700

    debug: test1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,10 +8,10 @@ function HomePage({setExerciseToEdit}) {
 
     //const API_BASE = process.env.VITE_API_URL; https://exercises-rest.onrender.com
     const API_BASE = "https://exercises-rest.onrender.com"
-    const test_API = import.meta.env.VITE_API_URL;
+    //const test_API = import.meta.env.VITE_API_URL;
 
     const loadExercises = async () => {
-        console.log(test_API)
+        //console.log(test_API)
         const response = await fetch(`${API_BASE}/exercises`);
         const exercises = await response.json();
         setExercises(exercises);
