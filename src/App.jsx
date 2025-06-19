0b826a3df0ca37dab82c0ce580b97975c2638092
Anil Verman <anilverman14@gmail.com>
2025-06-18 20:59:53 -0700

    fix(homepage): update wording in title

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <header>
       <h1>Exercise Tracker</h1>
       <p>Full Stack MERN App Demonstration</p>
-      <p>Verman Website</p>
+      <p>Verman Website wooty woot</p>
     </header>
     <div className='app'>
       <Router>
